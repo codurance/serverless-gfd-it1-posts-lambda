0bc00cad49befbe422b53100f270aae9ea05b227
Lau Llobet <lau.llobet@gmail.com>
2020-03-24 17:37:49 +0100

    Green: in local, needs permisions

--- handler.js
+++ handler.js
@@ -1,41 +1,43 @@
 'use strict';
 
 module.exports.hello = async event => {
-	var AWS = require('aws-sdk');
-	AWS.config.update({region: 'us-east-1'});
+  var AWS = require('aws-sdk');
+  AWS.config.update({ region: 'us-east-1' });
 
-// Create the DynamoDB service object
-var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+  // Create the DynamoDB service object
+  var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
-var params = {
-  TableName: 'serverless-gfd-it1-posts-database',
-  Key: {
-    'id': {S: '1234'}
-  },
-  ProjectionExpression: 'message'
-};
-
-// Call DynamoDB to read the item from the table
-ddb.getItem(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Item);
-  }
-});
-
-
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!'
-      },
-      null,
-      2
-    ),
+  var params = {
+    TableName: 'serverless-gfd-it1-posts-database',
+    Key: {
+      'id': { S: '1234' }
+    },
+    ProjectionExpression: 'message'
   };
+
+  return await new Promise((resolve, reject) => {
+    ddb.getItem(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify(
+            {
+              message: data.Item.message.S
+            },
+            null,
+            2
+          ),
+        })
+      }
+    });
+  });
+  // Call DynamoDB to read the item from the table
+
+
+
+
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
