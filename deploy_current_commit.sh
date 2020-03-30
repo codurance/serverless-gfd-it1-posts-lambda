5096a623d5e171483960148c1567ac23f397b657
Lau Llobet <lau.llobet@gmail.com>
2020-03-30 11:57:39 +0200

    Green: disable AWS pager in deployment script

--- deploy_current_commit.sh
+++ deploy_current_commit.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+export AWS_PAGER=""
 
-aws s3 rm s3://serverless-spike-dplyfrntnd-a-s3-1 --recursive 
-aws s3 rb s3://serverless-spike-dplyfrntnd-a-s3-1  
-aws s3api create-bucket --bucket serverless-spike-dplyfrntnd-a-s3-1 --region us-east-1  
+aws s3 rm s3://serverless-frontend-a-s3-3 --recursive 
+aws s3 rb s3://serverless-frontend-a-s3-3  
+aws s3api create-bucket --bucket serverless-frontend-a-s3-3 --region us-east-1  
 
-cd serverless-spike-dplyfrntnd-a-s3-1 
+cd serverless-frontend-a-s3-3 
+pwd
 yarn install
 yarn build 
 cd ..
 
-aws s3 cp ./serverless-spike-dplyfrntnd-a-s3-1/build s3://serverless-spike-dplyfrntnd-a-s3-1 --recursive  
-aws s3api put-bucket-policy --bucket serverless-spike-dplyfrntnd-a-s3-1 --policy file://web-policy.json  
-aws s3api put-bucket-cors --bucket serverless-spike-dplyfrntnd-a-s3-1 --cors-configuration file://cors-policy.json  
-open https://serverless-spike-dplyfrntnd-a-s3-1.s3.amazonaws.com/index.html
-aws s3 sync serverless-spike-dplyfrntnd-a-s3-1/build s3://serverless-spike-dplyfrntnd-a-s3-1/  --cache-control max-age=1
+aws s3 cp ./serverless-frontend-a-s3-3/build s3://serverless-frontend-a-s3-3 --recursive  
+aws s3api put-bucket-policy --bucket serverless-frontend-a-s3-3 --policy file://web-policy.json  
+aws s3api put-bucket-cors --bucket serverless-frontend-a-s3-3 --cors-configuration file://cors-policy.json  
+open https://serverless-frontend-a-s3-3.s3.amazonaws.com/index.html
+aws s3 sync serverless-frontend-a-s3-3/build s3://serverless-frontend-a-s3-3/  --cache-control max-age=1
