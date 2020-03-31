e175194c09ee873fbf26a4beffb97e689c41edf6
Javier Chacana <javier.chacana@codurance.com>
2020-03-31 15:42:52 +0200

    Green: test.bash successful for iteration 2

--- test.bash
+++ test.bash
@@ -1,2 +1,2 @@
-curl --write-out %{http_code} --silent --output /dev/null https://wt9b8pi1hh.execute-api.us-east-1.amazonaws.com/dev/message | grep 200 &
-curl https://wt9b8pi1hh.execute-api.us-east-1.amazonaws.com/dev/message | grep  "Fake"
+curl --write-out %{http_code} --silent --output /dev/null https://b1776kqi2k.execute-api.us-east-1.amazonaws.com/dev/message | grep 200 &
+curl https://b1776kqi2k.execute-api.us-east-1.amazonaws.com/dev/message | grep  "Fake"
